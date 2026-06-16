d33968aacbeb06fc6415366cbb13753032e0cce7
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-06-16 15:00:46 +0900

    newbranch

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,11 +1,1 @@
-def main() -> None:
-    name = "Rose"
-    height = 25
-    age = 30
-    print("=== Welcome to My Garden ===")
-    print(f"Plant: {name}")
-    print(f"Height: {height}cm")
-    print(f"Age: {age} days\n")
-    print("=== End of Program ===")
-    if __name__ == "__main__":
-        main()
+
