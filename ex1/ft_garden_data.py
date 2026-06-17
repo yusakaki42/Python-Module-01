80c68845dd9d64016d834d54e7554ed42815e6bf
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-06-17 22:23:21 +0900

    recording

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self, name:str, height:int, age:int):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+def main():
+	garden = [
+		Plant("Rose", 25, 30),
+		Plant("Tulip", 15, 20),
+		Plant("Lily", 40, 45)
+	]
+
+print("=== Garden Plant Registry ===")
+for plant in garden:
+	plant.show()
+
+if __name__ == "__main__":
+	main()
