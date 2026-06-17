4ee273123132ef9353715a5a32585362f899295b
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-06-17 16:39:31 +0900

    recording

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
 
+def main() -> None:
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age}days")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    main()
