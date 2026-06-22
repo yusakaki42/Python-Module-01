309716edf59754e6c745ef4d144ca3eb1cd45be1
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-06-22 13:48:18 +0900

    fix adding -> None

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, name:str, height:float, age:int):
+    def __init__(self, name:str, height:float, age:int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
-    def grow(self):
+    def grow(self) -> None:
         self.height = round(self.height + 0.8, 1)
         self.age += 1
 
-    def show(self):
+    def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
-def main():
+def main() -> None:
     height = 25.0
     rose = Plant("Rose", height, 30)
 
