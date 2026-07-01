2362afd06f52c13c45aaadf771be502e8f3625a9
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-07-01 19:59:29 +0900

    coding

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python3
 
-
 class Plant:
-    def __init__(self, name: str, height: float, age: int):
+    def __init__(self, name:str, height:float=0.0, age:int=0) -> None:
         self.name = name
-        self.height = height
-        self.age = age
+        self._height = height
+        self._age = age
+        # self.set_height(height)
+        # self.set_age(age)
 
-    def set_height(self, height: float) -> None:
+    def get_height(self) -> float:
+        return self._height
 
-        self.height = height
+    def get_age(self) -> int:
+        return self._age
+
+    def set_height(self, new_height:float) -> None:
+        if new_height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            print("Height update rejected")
+        else:
+            self._height = new_height
+            print(f"Height updated: {int(self._height)}cm")
+
+    def set_age(self, new_age:int) -> None:
+        if new_age < 0:
+            print(f"{self.name}: Error, age can't be negative")
+            print("Age update rejected")
+        else:
+            self._age = new_age
+            print(f"Age updated: {self._age} days")
+
+def main() -> None:
+    print("=== Garden Security System ===")
+    rose = Plant("Rose", 15.0, 10)
+    # print(f"Plant created: {rose.name}: {rose.get_height()}cm, {rose.get_age()} days old\n")
+    print(f"Plant created: {rose.name}: {rose._height}cm, {rose._age} days old\n")
+
+    rose.set_height(25.0)
+    rose.set_age(30)
+    print()
+    rose.set_height(-1.0)
+    rose.set_age(-1)
+    print()
+    print(f"Current state: {rose.name}: {rose.get_height()}cm, {rose.get_age()} days old")
+
+if __name__ == "__main__":
+    main()
