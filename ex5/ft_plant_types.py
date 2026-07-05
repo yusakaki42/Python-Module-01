8c62773df011cf66cd46563eafd7ce85a07a597c
Yu Sakakibara <yusakaki@c2r1s1.42tokyo.jp>
2026-07-05 22:22:28 +0900

    fix setter and getter

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -6,22 +6,22 @@ class Plant:
         self._height: float = height
         self._age: int = age
 
-    def set_height(self) -> float:
+    def get_height(self) -> float:
         return round(self._height, 1)
 
-    def set_age(self) -> int:
+    def get_age(self) -> int:
         return self._age
 
-    def get_height(self, new_height: float) -> None:
-        if self._height < 0.0:
+    def set_height(self, new_height: float) -> None:
+        if new_height < 0.0:
             print(f"{self._name}: Error, height can't be negative")
             print("Height update rejected")
         else:
             self._height = new_height
             print(f"Height updated: {self._height}cm")
 
-    def get_age(self, new_age: int) -> None:
-        if self._age < 0:
+    def set_age(self, new_age: int) -> None:
+        if new_age < 0:
             print(f"{self._name}: Error, age can't be negative")
             print("Age update rejected")
         else:
