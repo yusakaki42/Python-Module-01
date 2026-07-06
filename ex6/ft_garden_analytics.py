6fefc7b20461d0970e31b2afd798fcb3d0a66e88
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-07-06 20:10:19 +0900

    make stats

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 
+class Plant:
+    class _Stats:
+        def __init__(self):
+            self.__grow_count = 0
+            self.__age_count = 0
+            self.__show_count = 0
+
+    def __init__(self, name: str, height: float, age; int) -> None:
+        self._name: str = name
+        self._height: float = height
+        self._age: int = age
+        self._stats: Plant._Stats = Plant._Stats()
+
+
+def main(self):
+    print("=== Garden statistics ===")
+    print("=== Check year-old")
+    print(f"Is 30 days more than a year? -> {}")
+
+if __name__ == "__main__":
+    main()
